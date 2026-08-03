edef90fbc5e47fdf8620430a4ea8279d5123b418
saideshmukh3565-del <saideshmukh3565@gmail.com>
2026-08-03 09:38:33 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.Scanner;
+public class add
+{
+	public static void main(String arg[])
+	{
+		Scanner sc=new Scanner(System.in);
+			int a,b;
+		System.out("Enter A;");
+		a=sc.nextInt();
+		System.out.print("Enter B;");
+		b=sc.nextInt();
+		c=a+b;
+		{
+			System.out.println("Add is "+c);
+		}
+	}
+}
+
